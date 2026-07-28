c4ad73cbe0762089217f98372ddf8f9226e4fe0b
AdamMashaka <mashakaadam123@gmail.com>
2026-07-28 09:41:44 +0300

    cross

--- BACKEND/api/main.py
+++ BACKEND/api/main.py
@@ -15,7 +15,7 @@ api = FastAPI(title="Ecommerce Platform API", version="1.0.0")
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
