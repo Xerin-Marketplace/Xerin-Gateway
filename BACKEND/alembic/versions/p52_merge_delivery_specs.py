9749367cc12a592bd2d05d57599ceb3c9fb50903
AdamMashaka <mashakaadam123@gmail.com>
2026-09-04 09:47:00 +0300

    alembic

---
+++ BACKEND/alembic/versions/p52_merge_delivery_specs.py
@@ -0,0 +1,18 @@
+"""Merge delivery-evidence repair branch with product specifications.
+
+Revision ID: p52_merge_delivery_specs
+Revises: p51_category_product_specs, f6_delivery_evidence
+"""
+
+revision = "p52_merge_delivery_specs"
+down_revision = ("p51_category_product_specs", "f6_delivery_evidence")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
